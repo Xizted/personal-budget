4534c9849c5443e9fa38f45d9b836a094a145b2d
Osmar <xizted79@gmail.com>
2022-06-16 12:23:48 -0400

    [Backend] Added Middleware handler error and Notfound

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import dotenv from 'dotenv';
+import app from './app';
+dotenv.config();
+
+const PORT = process.env.NODE_ENV === 'test' ? 0 : process.env.PORT!;
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT} `);
+});
+
+export default server;
---
+++ backend/src/middleware/errorHandler.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime';
+import { NextFunction, Request, Response } from 'express';
+
+type PrismaError = PrismaClientKnownRequestError | PrismaClientValidationError;
+
+type PrismaErrorHandler = {
+  [key: string]: (error: PrismaClientKnownRequestError, res: Response) => void;
+};
+
+type ErroHandler = {
+  [key: string]: (error: Error, res: Response) => void;
+};
+
+const ERROR_HANDLER: ErroHandler = {
+  PASSWORD_NOT_SEND: (error: Error, res: Response) => {
+    res.status(422).send({ error: error.message });
+  },
+  DEFAULT_ERROR: (error: Error, res: Response) => {
+    res.status(500).send({ error: error.message, name: error.name });
+  },
+};
+
+const PRISMA_ERROR_HANDLER: PrismaErrorHandler = {
+  P2002: (error: PrismaClientKnownRequestError, res: Response) => {
+    const field: string | unknown =
+      error.meta?.target !== undefined ? error.meta.target : '';
+
+    res.status(400).send({
+      error: `The ${field} field must be unique`,
+    });
+  },
+
+  DEFAULT_ERROR:
+    () => (error: PrismaClientKnownRequestError, res: Response) => {
+      res.status(500).send({ error: error.message, name: error.name });
+    },
+};
+
+export default (
+  error: Error | PrismaError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let handler = undefined;
+  if (error instanceof PrismaClientKnownRequestError) {
+    handler =
+      PRISMA_ERROR_HANDLER[error.code] || PRISMA_ERROR_HANDLER['DEFAULT_ERROR'];
+    handler(error, res);
+  } else if (error instanceof PrismaClientValidationError) {
+    const start = error.message.search('Argument');
+    const end = error.message.search('missing.');
+    const message = error.message.slice(start, end + 7);
+
+    res.status(422).send({ error: message });
+  } else {
+    handler = ERROR_HANDLER[error.name] || ERROR_HANDLER['DEFAULT_ERROR'];
+    handler(error, res);
+  }
+};
