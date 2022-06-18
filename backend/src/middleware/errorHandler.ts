049124afa920b962a4a2bba62b133487251c795b
Osmar <xizted79@gmail.com>
2022-06-18 18:11:19 -0400

    [Backend] Add 404 error

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -4,6 +4,7 @@ import {
   PrismaClientValidationError,
 } from '@prisma/client/runtime';
 import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 type PrismaError = PrismaClientKnownRequestError | PrismaClientValidationError;
 
@@ -34,14 +35,28 @@ const PRISMA_ERROR_HANDLER: PrismaErrorHandler = {
     });
   },
 
-  DEFAULT_ERROR:
-    () => (error: PrismaClientKnownRequestError, res: Response) => {
-      res.status(500).send({ error: error.message, name: error.name });
-    },
+  P2025: (error: PrismaClientKnownRequestError, res: Response) => {
+    const message: string | unknown =
+      error.meta?.cause !== undefined ? error.meta.cause : '';
+
+    res.status(404).send({
+      error: message,
+    });
+  },
+
+  DEFAULT_ERROR: (error: PrismaClientKnownRequestError, res: Response) => {
+    console.log('owo');
+    res.status(500).send({ error: error.message, name: error.name });
+  },
 };
 
 export default (
-  error: Error | PrismaError,
+  error:
+    | Error
+    | PrismaError
+    | PrismaClientKnownRequestError
+    | PrismaClientValidationError
+    | ZodError,
   req: Request,
   res: Response,
   next: NextFunction
@@ -57,6 +72,8 @@ export default (
     const message = error.message.slice(start, end + 7);
 
     res.status(422).send({ error: message });
+  } else if (error instanceof ZodError) {
+    res.status(422).send({ error: error.issues });
   } else {
     handler = ERROR_HANDLER[error.name] || ERROR_HANDLER['DEFAULT_ERROR'];
     handler(error, res);
